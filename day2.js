e3d29a36ac793e86c24d218764b6efb6ad186035
reda <redaermich@gmail.com>
2022-03-02 00:24:53 +0100

    this another day.

--- day2.js
+++ day2.js
@@ -8,23 +8,23 @@
  - Use Variables b + d To Get This Valus
 */
 
-let a = 100;
-let b = 2 _00 .5;
-let c = 1e2;
-let d = 2.4;
+let a = 100,
+    b = 200.5,
+    c = 1e2,
+    d = 2.4;
 
 // Find Smallest Number In All Variables And Return Integer
-console.log();
+console.log(Math.floor(Math.min(a, b, c, d)));
 
 // Use Variables a + d One Time To Get The Needed Output
-console.log(); // 10000
+console.log((a * 100) + d * 0); // 10000
 
 // Get Integer "2" From d Variable With 4 Methods
-console.log();
-console.log();
-console.log();
-console.log();
+console.log(parseInt(String(d)));
+console.log(Math.trunc(d));
+console.log(Math.floor(d));
+console.log(Math.round(d));
 
 // Use Variables b + d To Get This Valus
-console.log(); // 66.67 => String
-console.log(); // 67 => Number
+console.log((Math.floor(b) / 3).toFixed(2) + d * 0) // 66.67 => String
+console.log(Math.floor(b / 3) + Math.trunc(d)) // 67 => Number
