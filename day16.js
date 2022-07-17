add203c8a21a9b95e5021a3e2a32e85004682a4d
ERMICH Reda <ermich.reda@ofppt-edu.ma>
2022-07-17 14:50:28 +0100

    initiale step array challenges

---
+++ day16.js
@@ -0,0 +1,15 @@
+/*
+  ***************  LONGEST WORD Challenge ***************
+  *
+  *Tasks:
+  ====================================
+    Function longestWord(str: string): string
+    Return the longest word of a string
+    ex. longestWord('Hi there, my name is Brad') === 'there,'
+*/
+
+const longestWord = str => {
+    // code
+}
+
+console.log(longestWord('Hi there, my name is Brad')); // 'there,'
---
+++ day17.js
@@ -0,0 +1,18 @@
+/*
+  ***************  ARRAY CHUNKING Challenge ***************
+  *
+  *Tasks:
+  ====================================
+    Function chunkArray(arr:array, len:int): array
+    Return array of arrays
+    Split an array into chunked arrays of a specific length.
+    ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
+    ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
+*/
+
+const chunkArray= (arr, len) => {
+    // code
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 3)); // [[1, 2, 3],[4, 5, 6],[7]]
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2)); // [[1, 2],[3, 4],[5, 6],[7]]
